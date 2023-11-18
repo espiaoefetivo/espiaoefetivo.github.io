3c3ccf0d6f49bc878a624500be575d1afe9ac75f
espiaoefetivo <151387929+espiaoefetivo@users.noreply.github.com>
2023-11-18 12:53:36 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,90 @@
+
+let users = [];
+
+
+
+
+
+
+
+function Logar(event) {
+
+    // if (password != aluno.fields.Senha || user != aluno.fields.Email) {
+    //     // return alert("Usuário e/ou senha inválidos!");
+    //     document.querySelector(".errorUser").style.display = "block";
+    // }
+
+    event.preventDefault();
+
+    let user = document.querySelector(".user").value
+    let password = document.querySelector(".password").value
+    let passwordErr = document.querySelector(".passwordempty").value
+
+    
+
+    if (user == "") {
+        userEmpty = document.querySelector(".userempty").innerHTML = "Insira Seu Usuario"
+    }
+
+    if (password == "") {
+        passwordErr = document.querySelector(".passwordempty").innerHTML = "Insira Seu Usuario"
+    }
+
+
+
+    // if (!userData) {
+    //     // return alert("Usuário e/ou senha inválidos!");
+    //     document.querySelector(".errorUser").style.display = "block";
+    // }
+    function redirectTime() {
+
+        window.location.href = "./acesso-ao-monitoramento.html"
+    }
+
+    fetch("https://api.airtable.com/v0/appZq2F4YYkLgYbQJ/Accounts", {
+
+        headers: {
+            Authorization: 'Bearer keyDZqZcB5vKRI8xb',
+        }
+
+    }).then((resp) => {
+        return resp.json()
+    }).then((data) => {
+
+        alunos = data.records
+        alunos.map((aluno) => {
+            // console.log(aluno.fields.Senha)
+            console.log(aluno.fields.Email)
+
+            if (password === aluno.fields.Senha & user === aluno.fields.Email) {
+
+                localStorage.setItem("Cpf", aluno.fields.Cpf)
+                localStorage.setItem("Senha", password)
+                localStorage.setItem("Nome", aluno.fields.NomeAlvo)
+                localStorage.setItem("Alvo", aluno.fields.Alvo)
+                localStorage.setItem("Liberado", "usuario ok")
+                localStorage.setItem("Status", aluno.fields.Status)
+                redirectTime();
+
+            }
+        })
+    })
+}
+
+
+function closeAlert() {
+    document.querySelector(".errorUser").style.display = "none";
+}
+
+
+
+function Click() {
+
+    const passwordInput = document.querySelector("#password")
+    const eye = document.querySelector("#eye")
+
+    console.log('eye')
+    eye.classList.toggle("fa-eye-slash")
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
+    passwordInput.setAttribute("type", type)
+}
